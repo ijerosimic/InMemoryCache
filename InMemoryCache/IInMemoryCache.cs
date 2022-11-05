e1b1213e50cfe7e2cb3e65cbc2888e488612ab7a
ijerosimic <igor.jerosimic1@gmail.com>
2022-11-05 21:21:43 +0000

    Init

---
+++ InMemoryCache/IInMemoryCache.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace InMemoryCache;
+
+public interface IInMemoryCache<T> where T : class
+{
+    T? Get(string key);
+    void Set(string key, T value, out string? evictedValue);
+    int GetCount();
+    int GetThreshold();
+}
+
+public class InMemoryCache<T> : IInMemoryCache<T> where T : class
+{
+    private readonly LinkedList<string> _keysQueue;
+    private readonly Dictionary<string, LinkedListNode<(string key, T? value)>> _cache;
+    private readonly object _obj = new();
+    private readonly int _maxItemsCount;
+
+    public InMemoryCache(IOptions<InMemoryCacheOptions> cacheOptions)
+    {
+        var configuredMax = cacheOptions.Value?.MaxItems;
+        _maxItemsCount = configuredMax is >= 1
+            ? configuredMax.Value
+            : throw new ArgumentOutOfRangeException(nameof(InMemoryCacheOptions.MaxItems),
+                "Invalid limit for max cache storage. Must be >= 1.");
+        _keysQueue = new LinkedList<string>();
+        _cache = new Dictionary<string, LinkedListNode<(string, T?)>>();
+    }
+
+    public int GetThreshold() => _maxItemsCount;
+
+    public int GetCount()
+    {
+        lock (_obj)
+            return _cache.Count;
+    }
+
+    public T? Get(string key)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+            throw new ArgumentNullException(nameof(key), "Cache key must have a value.");
+
+        lock (_obj)
+        {
+            if (!_cache.ContainsKey(key))
+                return null;
+
+            var node = _cache[key];
+            _keysQueue.Remove(node.Value.key);
+            _keysQueue.AddFirst(node.Value.key);
+            
+            return node.Value.value;
+        }
+    }
+
+    public void Set(string key, T value, out string? evictedKey)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+            throw new ArgumentNullException(nameof(key), "Cache key must have a value.");
+
+        if (value is null)
+            throw new ArgumentNullException(nameof(value), "Cannot cache null value.");
+
+        lock (_obj)
+        {
+            evictedKey = null;
+
+            if (_cache.ContainsKey(key))
+            {
+                var cachedNode = _cache[key];
+                _keysQueue.Remove(cachedNode.Value.key);
+                _keysQueue.AddFirst(cachedNode.Value.key);
+                _cache[key] = cachedNode;
+            }
+
+            var node = value;
+            if (_cache.Count >= _maxItemsCount)
+            {
+                var last = _keysQueue.Last;
+                _cache.Remove(last!.Value);
+                _keysQueue.RemoveLast();
+                evictedKey = last.Value;
+            }
+
+            _cache[key] = new LinkedListNode<(string key, T? value)>((key, value));
+            _keysQueue.AddFirst(key);
+        }
+    }
+}
+
+public class InMemoryCacheOptions
+{
+    public int MaxItems { get; init; }
+}
---
+++ InMemoryCacheTests/InMemoryCacheTests.cs
@@ -0,0 +1,121 @@
+using FluentAssertions;
+using InMemoryCache;
+using Microsoft.Extensions.Options;
+
+namespace InMemoryCacheTests;
+
+public class InMemoryCacheTests
+{
+    private readonly (string key, object value) _kvp1 = ("key-1", new { Val = "value-1" });
+    private readonly (string key, object value) _kvp2 = ("key-2", new { Val = "value-2" });
+    private readonly (string key, object value) _kvp3 = ("key-3", new { Val = "value-3" });
+    private readonly (string key, object value) _kvp4 = ("key-4", new { Val = "value-4" });
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void It_Throws_ArgumentOutOfRange_Exception_When_MaxItems_Param_Is_Invalid(int maxItems)
+    {
+        var mockOptions = Options.Create(new InMemoryCacheOptions { MaxItems = maxItems });
+        var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new InMemoryCache<object>(mockOptions));
+        ex.Message.Should().Contain("Invalid limit for max cache storage. Must be >= 1.");
+    }
+
+    [Fact]
+    public void It_Sets_The_Item_In_The_Memory_Cache()
+    {
+        var mockOptions = Options.Create(new InMemoryCacheOptions { MaxItems = 5 });
+        var sut = new InMemoryCache<object>(mockOptions);
+
+        sut.Set(_kvp1.key, _kvp1.value, out _);
+        var get = sut.Get(_kvp1.key);
+        sut.Get(_kvp1.key).Should().Be(_kvp1.value);
+        sut.GetCount().Should().Be(1);
+        sut.GetThreshold().Should().Be(5);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void It_Throws_ArgumentNullException_When_Setting_An_Item_With_An_Invalid_Key(string key)
+    {
+        var mockOptions = Options.Create(new InMemoryCacheOptions { MaxItems = 5 });
+        var sut = new InMemoryCache<object>(mockOptions);
+
+        var ex = Assert.Throws<ArgumentNullException>(() => sut.Set(key, _kvp1.value, out _));
+        ex.Message.Should().Contain("Cache key must have a value.");
+    }
+
+    [Fact]
+    public void It_Throws_ArgumentNullException_When_Setting_An_Item_With_An_Invalid_Value()
+    {
+        var mockOptions = Options.Create(new InMemoryCacheOptions { MaxItems = 5 });
+        var sut = new InMemoryCache<object>(mockOptions);
+
+        object? invalidValue = null;
+        var ex = Assert.Throws<ArgumentNullException>(() => sut.Set(_kvp1.key, invalidValue!, out _));
+        ex.Message.Should().Contain("Cannot cache null value.");
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void It_Throws_ArgumentNullException_When_Getting_An_Item_With_An_Invalid_Key(string key)
+    {
+        var mockOptions = Options.Create(new InMemoryCacheOptions { MaxItems = 5 });
+        var sut = new InMemoryCache<object>(mockOptions);
+
+        var ex = Assert.Throws<ArgumentNullException>(() => sut.Get(key));
+        ex.Message.Should().Contain("Cache key must have a value.");
+    }
+
+    [Fact]
+    public void It_Returns_Null_When_Calling_Get_On_Empty_Cache()
+    {
+        var mockOptions = Options.Create(new InMemoryCacheOptions { MaxItems = 5 });
+        var sut = new InMemoryCache<object>(mockOptions);
+
+        sut.Get(_kvp1.key).Should().BeNull();
+    }
+
+    [Fact]
+    public void It_Returns_Null_When_Calling_Get_With_Nonexistent_Key()
+    {
+        var mockOptions = Options.Create(new InMemoryCacheOptions { MaxItems = 5 });
+        var sut = new InMemoryCache<object>(mockOptions);
+
+        sut.Set(_kvp1.key, _kvp1.value, out _);
+
+        sut.Get("non-existent-key").Should().BeNull();
+    }
+
+    [Fact]
+    public void It_Evicts_The_Least_Recently_Used_Item_When_Setting_New_Item_Above_Threshold()
+    {
+        var mockOptions = Options.Create(new InMemoryCacheOptions { MaxItems = 3 });
+        var sut = new InMemoryCache<object>(mockOptions);
+
+        sut.Set(_kvp1.key, _kvp1.value, out _);
+        sut.Set(_kvp2.key, _kvp2.value, out _);
+        sut.Set(_kvp3.key, _kvp3.value, out _);
+
+        // Refresh positions
+        sut.Get(_kvp1.key).Should().Be(_kvp1.value);
+        sut.Get(_kvp2.key).Should().Be(_kvp2.value);
+
+        // Set new item - trigger LRU eviction for "key-3"
+        sut.Set(_kvp4.key, _kvp4.value, out _);
+
+        sut.GetCount().Should().Be(3);
+        sut.GetThreshold().Should().Be(3);
+
+        // LRU is evicted
+        sut.Get(_kvp3.key).Should().BeNull();
+
+        sut.Get(_kvp1.key).Should().Be(_kvp1.value);
+        sut.Get(_kvp2.key).Should().Be(_kvp2.value);
+        sut.Get(_kvp4.key).Should().Be(_kvp4.value);
+    }
+}
